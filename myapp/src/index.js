7e964c1aad26c2f5d9d695b94e6da969465a3243
winterbreaker23 <johnjamesdev@hotmail.com>
2023-05-31 20:03:58 +0100

    Multiple Input Fields

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -42,9 +42,46 @@ function MyForm2() {
   )
 }
 
+function MyForm3() {
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(inputs);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Enter your name:
+      <input 
+        type="text" 
+        name="username" 
+        value={inputs.username || ""} 
+        onChange={handleChange}
+      />
+      </label>
+      <label>Enter your age:
+        <input 
+          type="number" 
+          name="age" 
+          value={inputs.age || ""} 
+          onChange={handleChange}
+        />
+        </label>
+        <input type="submit" />
+    </form>
+  )
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <MyForm2/>
+  <MyForm3/>
 );
 
 // If you want to start measuring performance in your app, pass a function
