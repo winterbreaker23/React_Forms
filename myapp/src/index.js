61fdb547895156c53bba1cc889077adc7fdded3f
winterbreaker23 <johnjamesdev@hotmail.com>
2023-05-31 20:06:52 +0100

    Select

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -95,9 +95,28 @@ function MyForm4() {
   )
 }
 
+function MyForm5() {
+  const [myCar, setMyCar] = useState("Volvo");
+
+  const handleChange = (event) => {
+    setMyCar(event.target.value)
+  }
+
+  return (
+    <form>
+      <select value={myCar} onChange={handleChange}>
+        <option value="Ford">Ford</option>
+        <option value="Volvo">Volvo</option>
+        <option value="Fiat">Fiat</option>
+      </select>
+    </form>
+  )
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <MyForm4/>
+  <MyForm5/>
 );
 
 // If you want to start measuring performance in your app, pass a function
