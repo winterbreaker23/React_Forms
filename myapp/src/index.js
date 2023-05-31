8593b7cd98c911186fdbee137c93439350d877a1
winterbreaker23 <johnjamesdev@hotmail.com>
2023-05-31 19:50:43 +0100

    Handling Forms

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -3,12 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { useState } from 'react';
+function MyForm() {
+  const [name, setName] = useState("");
 
+  return (
+    <form>
+      <label>Enter your name:
+        <input
+          type="text" 
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+    </form>
+  )
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <MyForm/>
 );
 
 // If you want to start measuring performance in your app, pass a function
